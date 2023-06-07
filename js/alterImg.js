13c49ef37a8215c15f6042c8e0c79e1e2f725fa9
Mauricio Madrid Prado <mauriciomadridprado@gmail.com>
2023-06-07 19:41:03 -0300

    fixed: console log in alterImg

--- js/alterImg.js
+++ js/alterImg.js
@@ -5,8 +5,8 @@ let l;
 export function alterImg() {
 
      (n == 4) ? n = 1: +n++;
-     (n == 2 || n == 4) ? l = 'X' : l = 'Y' 
+     (n == 2 || n == 4) ? l = 'X' : l = 'Y' ;
     
-     console.log(n)
+
     addImage(n, l);
   }
